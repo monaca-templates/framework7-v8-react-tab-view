0548e611d887d8141434c58dd827015f14ebebdc
yong <yong@asial.co.jp>
2023-06-10 13:53:45 +0900

    init

---
+++ vite.config.js
@@ -0,0 +1,39 @@
+
+import path from 'path';
+import react from '@vitejs/plugin-react';
+
+
+const SRC_DIR = path.resolve(__dirname, './src');
+const PUBLIC_DIR = path.resolve(__dirname, './public');
+const BUILD_DIR = path.resolve(__dirname, './www',);
+const HOST = process.env.MONACA_SERVER_HOST || '0.0.0.0';
+export default async () => {
+
+  return  {
+    plugins: [
+      react(),
+
+    ],
+    root: SRC_DIR,
+    base: '',
+    publicDir: PUBLIC_DIR,
+    build: {
+      outDir: BUILD_DIR,
+      assetsInlineLimit: 0,
+      emptyOutDir: false,
+      rollupOptions: {
+        treeshake: false,
+      },
+    },
+    resolve: {
+      alias: {
+        '@': SRC_DIR,
+      },
+    },
+  server: {
+    host: HOST,
+    port: 8080,
+  },
+
+  };
+}
